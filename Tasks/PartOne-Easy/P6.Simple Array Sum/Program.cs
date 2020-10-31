71ad6bf8e33411a0779ef2a9510fdec0598de87c
Vencislav Kolev <venci362@gmail.com>
2020-10-31 21:59:47 +0200

    solved

---
+++ Tasks/PartOne-Easy/P6.Simple Array Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar)
+    {
+        int sum = 0;
+        foreach (var element in ar)
+        {
+            sum += element;
+        }
+        return sum;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
