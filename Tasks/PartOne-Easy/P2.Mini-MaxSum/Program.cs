589cfc7e823fcea5b46d8c32c89bfa80b41a337a
Vencislav Kolev <venci362@gmail.com>
2020-10-31 17:59:48 +0200

    solved

---
+++ Tasks/PartOne-Easy/P2.Mini-MaxSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr)
+    {
+        long sum = 0;
+        int min = arr.Min();
+        int max = arr.Max();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        Console.WriteLine("{0} {1}", sum - max, sum - min);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
+    }
+}
---
+++ Tasks/PartOne-Easy/P4.TimeConversion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        string[] timeArgs = s.Split(':');
+        string dayPart = timeArgs[timeArgs.Length - 1].Substring(2);
+
+        int hours = int.Parse(timeArgs[0]);
+        string minutes = timeArgs[1];
+        string seconds = timeArgs[2].Substring(0, 2);
+
+        if (dayPart == "AM")
+        {
+            if (hours == 12)
+            {
+                hours -= 12;
+            }
+        }
+        else if (dayPart == "PM")
+        {
+            if (hours < 12)
+            {
+                hours += 12;
+            }
+        }
+        return $"{hours:d2}:{minutes}:{seconds}";
+    }
+
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+        Console.WriteLine(result);
+    }
+}
