eafadd608eaba8d569d7bfe6ffbd38eaf556b262
Vencislav Kolev <venci362@gmail.com>
2020-11-06 23:21:12 +0200

    solved

---
+++ Tasks/PartTwo/P1.Apple and Orange/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Solution
+{
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+    {
+        int appleTreePos = a;
+        int orangeTreePos = b;
+        int validApples = 0;
+        int validOranges = 0;
+
+        foreach (var position in apples)
+        {
+            int currLocation = appleTreePos + position;
+            if (currLocation >= s && currLocation <= t)
+            {
+                validApples++;
+            }
+        }
+        foreach (var position in oranges)
+        {
+            int currLocation = orangeTreePos + position;
+            if (currLocation >= s && currLocation <= t)
+            {
+                validOranges++;
+            }
+        }
+        Console.WriteLine(validApples);
+        Console.WriteLine(validOranges);
+    }
+
+    static void Main(string[] args)
+    {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+        ;
+
+        int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
---
+++ Tasks/PartTwo/P2.Number Line Jumps/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Solution
+{
+
+    // Complete the kangaroo function below.
+    static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        //x1 + y * v1 = x2 + y * v2 where "y" is number of jumps...
+        string result = string.Empty;
+        if (v1 > v2 && (x2 - x1) % (v2 - v1) == 0)
+        {
+            result = "YES";
+        }
+        else
+        {
+            result = "NO";
+        }
+        //while (result == string.Empty)
+        //{
+        //    x1 += v1;
+        //    x2 += v2;
+        //    if (x1 == x2)
+        //    {
+        //        result = "YES";
+        //    }
+        //    else if (x1 + v1 < x2 || x2 + v2 < x1)
+        //    {
+        //        result = "NO";
+        //    }
+        //}
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+
+        string[] x1V1X2V2 = Console.ReadLine().Split(' ');
+
+        int x1 = Convert.ToInt32(x1V1X2V2[0]);
+
+        int v1 = Convert.ToInt32(x1V1X2V2[1]);
+
+        int x2 = Convert.ToInt32(x1V1X2V2[2]);
+
+        int v2 = Convert.ToInt32(x1V1X2V2[3]);
+
+        string result = kangaroo(x1, v1, x2, v2);
+
+        Console.WriteLine(result);
+    }
+}
