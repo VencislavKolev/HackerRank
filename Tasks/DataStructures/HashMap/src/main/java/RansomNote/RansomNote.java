9addec7018ebef7dd693f98abbfb86ba770deee4
VencislavKolev <venci362@gmail.com>
2021-05-24 20:59:05 +0300

    Add RansomNote

---
+++ Tasks/DataStructures/HashMap/src/main/java/RansomNote/RansomNote.java
@@ -0,0 +1,84 @@
+package RansomNote;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'checkMagazine' function below.
+     *
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY magazine
+     *  2. STRING_ARRAY note
+     */
+
+    public static void checkMagazine(List<String> magazine, List<String> note) {
+        // Write your code here
+        Map<String, Integer> magazineMap = new HashMap<>();
+        for (String word : magazine) {
+            if (!magazineMap.containsKey(word)) {
+                magazineMap.put(word, 1);
+            } else {
+                magazineMap.replace(word, magazineMap.get(word) + 1);
+            }
+        }
+
+        Map<String, Integer> notMap = new HashMap<>();
+        for (String word : note) {
+            if (!notMap.containsKey(word)) {
+                notMap.put(word, 1);
+            } else {
+                notMap.replace(word, notMap.get(word) + 1);
+            }
+        }
+
+        boolean canBeFormed = true;
+        for (Map.Entry<String, Integer> kvp : notMap.entrySet()) {
+            String currentWord = kvp.getKey();
+            Integer requiredCount = kvp.getValue();
+            if (magazineMap.containsKey(currentWord) && magazineMap.get(currentWord) >= requiredCount) {
+                magazineMap.remove(currentWord);
+            } else {
+                canBeFormed = false;
+            }
+        }
+        if (canBeFormed) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+
+    }
+
+}
+
+class RansomNote {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(firstMultipleInput[0]);
+
+        int n = Integer.parseInt(firstMultipleInput[1]);
+
+        List<String> magazine = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+
+        List<String> note = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+
+        Result.checkMagazine(magazine, note);
+
+        bufferedReader.close();
+    }
+}
