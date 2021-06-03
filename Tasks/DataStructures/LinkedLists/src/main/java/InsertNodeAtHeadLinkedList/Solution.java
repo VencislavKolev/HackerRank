c3f6260bcd3a1b7419ce0f988b1804c1e120a989
VencislavKolev <venci362@gmail.com>
2021-06-03 21:44:40 +0300

    fix OutputStream

--- Tasks/DataStructures/LinkedLists/src/main/java/InsertNodeAtHeadLinkedList/Solution.java
+++ Tasks/DataStructures/LinkedLists/src/main/java/InsertNodeAtHeadLinkedList/Solution.java
@@ -1,8 +1,8 @@
 package InsertNodeAtHeadLinkedList;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Solution {
@@ -56,7 +56,7 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         SinglyLinkedList llist = new SinglyLinkedList();
 
