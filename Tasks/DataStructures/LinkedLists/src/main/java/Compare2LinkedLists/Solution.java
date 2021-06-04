255ba323ffd9deef73005b884734e20b420461f0
VencislavKolev <venci362@gmail.com>
2021-06-04 17:45:56 +0300

    Add Compare 2 Linked Lists Recursive solution

--- Tasks/DataStructures/LinkedLists/src/main/java/Compare2LinkedLists/Solution.java
+++ Tasks/DataStructures/LinkedLists/src/main/java/Compare2LinkedLists/Solution.java
@@ -52,19 +52,34 @@ public class Solution {
     }
 
     static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        while (true) {
-            if (head1 == null && head2 == null) {
-                return true;
-            }
-            if (head1 == null || head2 == null) {
-                return false;
-            }
-            if (head1.data != head2.data) {
-                return false;
-            }
-            head1 = head1.next;
-            head2 = head2.next;
+        // ITERATIVE WAY
+//        while (true) {
+//            if (head1 == null && head2 == null) {
+//                return true;
+//            }
+//            if (head1 == null || head2 == null) {
+//                return false;
+//            }
+//            if (head1.data != head2.data) {
+//                return false;
+//            }
+//            head1 = head1.next;
+//            head2 = head2.next;
+//        }
+
+        // RECURSIVE WAY
+        if (head1 == null && head2 == null) {
+            return true;
         }
+        if (head1 == null || head2 == null) {
+            return false;
+        }
+        if (head1.data != head2.data) {
+            return false;
+        }
+        return compareLists(head1.next, head2.next);
+
+
     }
 
     private static final Scanner scanner = new Scanner(System.in);
