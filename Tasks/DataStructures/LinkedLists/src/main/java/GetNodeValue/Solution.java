d6ab938800c0d6286445e079c9f229f3a09c9f57
VencislavKolev <venci362@gmail.com>
2021-06-08 11:43:59 +0300

    Add Get Node Value In LinkedList

---
+++ Tasks/DataStructures/LinkedLists/src/main/java/GetNodeValue/Solution.java
@@ -0,0 +1,139 @@
+package GetNodeValue;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class SinglyLinkedListNode {
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    public SinglyLinkedListNode head;
+    public SinglyLinkedListNode tail;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertNode(int nodeData) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+}
+
+class SinglyLinkedListPrintHelper {
+    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'getNode' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_SINGLY_LINKED_LIST llist
+     *  2. INTEGER positionFromTail
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static int getNode(SinglyLinkedListNode llist, int positionFromTail) {
+        //T - O(n) // S - O(n)
+//        Map<Integer, SinglyLinkedListNode> map = new HashMap<>();
+//        SinglyLinkedListNode current = llist;
+//        int count = 0;
+//        while (current != null) {
+//            map.put(count++, current);
+//            current = current.next;
+//        }
+//        return map.get(count - positionFromTail).data;
+
+        SinglyLinkedListNode pointer = llist;
+        int counter = 0;
+        // pointer starts walking when is exactly positionsFromTail positions from the head.
+        // This means when head is at the end, pointer will be at positionsFromTail node.
+        while (llist != null) {
+            llist = llist.next;
+            if (counter <= positionFromTail) {
+                counter++;
+            } else {
+                pointer = pointer.next;
+            }
+        }
+        return pointer.data;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tests = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, tests).forEach(testsItr -> {
+            try {
+                SinglyLinkedList llist = new SinglyLinkedList();
+
+                int llistCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+                IntStream.range(0, llistCount).forEach(i -> {
+                    try {
+                        int llistItem = Integer.parseInt(bufferedReader.readLine().trim());
+
+                        llist.insertNode(llistItem);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                int position = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.getNode(llist.head, position);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
